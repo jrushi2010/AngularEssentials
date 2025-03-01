e8c4fb7353904e37b4aaab855229990782231261
jrushi2010 <jrushi2010@gmail.com>
2025-03-01 19:20:06 +0530

    Outputting user specific tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, UserComponent, TasksComponent],
+  declarations: [AppComponent, HeaderComponent, UserComponent, TasksComponent, TaskComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
