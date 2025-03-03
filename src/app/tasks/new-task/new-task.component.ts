33fc3a1a706021c95eae8562ef599be85676915b
jrushi2010 <jrushi2010@gmail.com>
2025-03-03 15:57:05 +0530

    Managing the cancel button on the dialog box.

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-new-task',
   templateUrl: './new-task.component.html',
-  styleUrls: ['./new-task.component.css']
+  styleUrls: ['./new-task.component.css'],
 })
 export class NewTaskComponent {
+  @Output() cancel = new EventEmitter<void>();
 
+  onCancel() {
+    this.cancel.emit();
+  }
 }
