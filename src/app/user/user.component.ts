15d157d89ef46cddfc6fd8d3694f28c49884a447
jrushi2010 <jrushi2010@gmail.com>
2025-03-03 15:27:48 +0530

    storing data models in saperate files

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,16 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
-
-// type User = {
-//   id: string;
-//   avatar: string;
-//   name: string;
-// }
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { User } from './user.model';
 
 @Component({
   selector: 'app-user',
